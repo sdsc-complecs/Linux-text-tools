f46f70f7f5447eec5a725b47f2ca1f227e101327
sinkovit <sinkovit@sdsc.edu>
2025-09-04 10:18:37 -0700

    Modernized script

--- CaseStudyHPL/process-hpl.sh
+++ CaseStudyHPL/process-hpl.sh
@@ -1,6 +1,5 @@
 echo 'node      time    P F'                   # Print header
-for file in $(ls output.hpl*)                  # Iterate over output files
-do
+for file in $(ls output.hpl*); do              # Iterate over output files
   node=$(head -1 $file)                        # Node name from first line
   t=$(awk '/WR12R2R4/ {print $6}' $file)       # Time from 6th field of WR12R2R4 line
   npass=$(grep PASSED $file| wc -l)            # Number of passed tests
