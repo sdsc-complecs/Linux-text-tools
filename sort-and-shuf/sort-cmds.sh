002374c122983a8d5988037a8258dfd6515e14e4
sinkovit <sinkovit@sdsc.edu>
2025-09-04 10:08:39 -0700

    Modernizes script, make sort more robust

--- sort-and-shuf/sort-cmds.sh
+++ sort-and-shuf/sort-cmds.sh
@@ -1,8 +1,8 @@
 sort unsorted1.txt
 export LC_ALL=C; sort unsorted1.txt; export LC_ALL=''
-sort -k3 unsorted2.txt
-sort -k2 unsorted2.txt
-sort -k2 -n unsorted2.txt
+sort -k3,3 unsorted2.txt
+sort -k2,2 unsorted2.txt
+sort -k2,2n unsorted2.txt
 sort -u unsorted1.txt
 sort -k3,3 -k1,1 -k2,2 unsorted3.txt
 sort -k2r,2 -k1,1 -k3,3 unsorted3.txt
