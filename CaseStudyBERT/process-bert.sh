ff954badc6670933b7a7917eae1c3d6ad80294c5
sinkovit <sinkovit@sdsc.edu>
2025-09-04 10:30:32 -0700

    Modernizes script

--- CaseStudyBERT/process-bert.sh
+++ CaseStudyBERT/process-bert.sh
@@ -1,11 +1,10 @@
-for file in `ls -1 out*`
-do
-  node=`head -1 $file | awk '{print $1}'`
-  card=`grep 'AIP (' $file | awk '{print $4}'`
-  tstart=`grep TBEFORE $file | awk '{print $2}'`
-  tend=`grep AFTER $file | awk '{print $2}'`
-  corr=`grep 'Correct answers' $file | tail -1 | awk '{print $8}'`   # Last instance
-  ach=`grep 'Achieved sentences' $file | tail -1 | awk '{print $3}'` # Last instance
+for file in $(ls -1 out*); do
+  node=$(head -1 $file | awk '{print $1}')
+  card=$(awk '/AIP \(/ {print $4}' $file)
+  tstart=$(awk '/TBEFORE/ {print $2}' $file)
+  tend=$(awk '/AFTER/ {print $2}' $file)
+  corr=$(awk '/Correct answers/ {print $8}' $file | tail -n 1)   # Last instance
+  ach=$(awk '/Achieved sentences/ {print $3}' $file | tail -n 1) # Last instance
   t=$(expr $tend - $tstart)                     # Do math to calculate tend - tstart
   echo $node $card $corr $ach $t
 done
